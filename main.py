edd90e6dc8e1a47ab90537ee2b796b7f0d4a0dd5
Ricardo <102841004+ricardolima29@users.noreply.github.com>
2022-09-07 22:03:27 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+from asyncio.proactor_events import _ProactorDuplexPipeTransport
+from email.mime import audio
+from time import strftime
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import pywhatkit
+
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
+def executa_comando():
+    try:
+        with sr.Microphone() as source:
+            print('Ouvindo..')
+            voz = audio.listen(source)
+            comando = audio.recognize_google(voz, language='pt-BR')
+            comando = comando.lower()
+            if 'tina' in comando:
+                comando = comando.replace('tina','' )
+                maquina.say(comando)
+                maquina.runAndWait()
+
+    except:
+        print('Microfone nao esta ok')
+    return comando
+
+
+def comando_voz_usuario():
+    comando = executa_comando()
+    if 'horas' in comando:
+        hora = datetime.datetime.now().strftime('%H:%M')
+        maquina.say('Agora são' + hora)
+        maquina.runAndWait()
+    elif 'procure por' in comando:
+        procurar = comando.replace('procure por', '')
+        wikipedia.set_lang('pt')
+        resultado = wikipedia.summary(procurar,2)
+        print(resultado)
+        maquina.say(resultado)
+        maquina.runAndWait()
+    elif 'toque' in comando:
+        musica = comando.replace('toque','')
+        resultado = pywhatkit.playonyt(musica)
+        maquina.say('Tocando música')
+        maquina.runAndWait()
+
+comando_voz_usuario()
